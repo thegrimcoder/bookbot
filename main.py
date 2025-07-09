e25b0e85961cdef766c55beef29903fc4e1cf880
thegrimcoder <mbrust035@gmail.com>
2025-07-08 21:43:34 -0500

    Added get_num_character

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,6 +8,8 @@ def get_book_text(filepath):
 
 def main():
     total_words = get_num_words(get_book_text("./books/frankenstein.txt"))
+    total_characters = get_num_characters(get_book_text("./books/frankenstein.txt"))
     print(f"{total_words} words found in the document")
+    print(total_characters)
     
 main()                
