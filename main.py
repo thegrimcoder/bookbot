94b5e186cf16b6d19c1fefab8ad1083f22a5aa42
thegrimcoder <mbrust035@gmail.com>
2025-07-10 12:55:15 -0500

    Update main function in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, sort_dictionary
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,8 +8,9 @@ def get_book_text(filepath):
 
 def main():
     total_words = get_num_words(get_book_text("./books/frankenstein.txt"))
-    total_characters = get_num_characters(get_book_text("./books/frankenstein.txt"))
+    character_dictionary = get_num_characters(get_book_text("./books/frankenstein.txt"))
+    sorted_list = sort_dictionary(character_dictionary)
     print(f"{total_words} words found in the document")
-    print(total_characters)
+    print(sorted_list)
     
 main()                
