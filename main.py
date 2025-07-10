5545073f99354be135d68c93cdd60e2e67d00e6e
thegrimcoder <mbrust035@gmail.com>
2025-07-10 13:30:05 -0500

    Update main function to print report main.py

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ def main():
     total_words = get_num_words(get_book_text("./books/frankenstein.txt"))
     character_dictionary = get_num_characters(get_book_text("./books/frankenstein.txt"))
     sorted_list = sort_dictionary(character_dictionary)
-    print(f"{total_words} words found in the document")
-    print(sorted_list)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    for key, value in sorted_list:
+        if key.isalpha():
+            print(f{key} : {value})
+    print("============= END ===============")
     
 main()                
